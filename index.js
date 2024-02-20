560938a09eb413ee1eaf261b191c7c9b8d41ac03
xiaofei <122727418+xiaozhou26@users.noreply.github.com>
2024-02-20 21:00:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,37 +3,41 @@ const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-let targetURLs = process.env.TARGET_URLS ? process.env.TARGET_URLS.split(',') : ['https://api.deeplx.org'];
+const targetURLs = process.env.TARGET_URLS ? process.env.TARGET_URLS.split(',') : ['https://api.deeplx.org'];
 
 app.use(express.json());
 app.use(express.text());
 
 app.all('*', async (req, res) => {
-    let response;
+    // 创建请求配置，考虑设置请求超时
+    const createRequestConfig = (targetURL) => ({
+        method: req.method,
+        url: targetURL + req.path,
+        data: req.method !== 'GET' ? req.body : null,
+        timeout: 5000, // 设置5秒超时
+    });
+
+    // 封装请求以捕获并处理单个请求的错误
+    const makeRequest = async (config) => {
+        try {
+            return await axios(config);
+        } catch (error) {
+            return Promise.reject(`请求到 ${config.url} 失败: ${error.message}`);
+        }
+    };
+
     try {
-        response = await Promise.any(targetURLs.map(async (targetURL) => {
-            const userURI = req.path;
-            const proxyURL = targetURL + userURI;
+        const responses = await Promise.any(targetURLs.map(targetURL => makeRequest(createRequestConfig(targetURL))));
 
-            return await axios({
-                method: req.method,
-                url: proxyURL,
-                data: req.method !== 'GET' ? req.body : null,
-            });
-        }));
-    } catch (error) {
-        console.error('Error during fetch:', error);
-        return res.status(500).send('所有目标URL都无法获取有效响应');
-    }
-
-    if (response && response.status >= 200 && response.status < 300) {
-        // 设置一个固定的 Content-Type，例如 'application/octet-stream'
+        // 响应成功，设置响应头并返回数据
         res.set({
             'Access-Control-Allow-Origin': '*',
-            'Content-Type': 'application/octet-stream', // 或者根据你的需求选择其他类型
-        }).status(response.status).send(response.data);
-    } else {
-        res.status(500).send('所有目标URL都无法获取有效响应');
+            'Content-Type': 'application/json', // 根据需要调整Content-Type
+        }).status(responses.status).send(responses.data);
+
+    } catch (error) {
+        // 所有请求都失败了
+        res.status(500);
     }
 });
 
